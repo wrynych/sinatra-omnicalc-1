60e66c9f0aa9e26a3d2271b13b0339689e6f008d
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-02-14 06:07:09 +0000

    ch 2

--- app.rb
+++ app.rb
@@ -5,9 +5,22 @@ get("/howdy") do
   erb(:hello)
 end
 
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square/new") do
+  erb(:square_form)
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
 end
